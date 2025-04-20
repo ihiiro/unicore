b748d5d968f4ef3f2a2173e6a952c5433b751513
Yassir El yaqine <yel-yaqi@e3r7p12.1337.ma>
2025-04-20 17:17:10 +0100

    @ more FSM states implemented for unicore_http_parse_start_line() @

--- core/unicore_defines.hpp
+++ core/unicore_defines.hpp
@@ -3,8 +3,39 @@
 #pragma once
 
 
+/* COMMON BYTES */
+#ifndef LF
 #define LF 0x0A
+#endif
+
+#ifndef CR
 #define CR 0x0D
+#endif
+
+#ifndef SP
+#define SP 0x20
+#endif
+
+/* STATUS CODE CATEGORIES */
+#ifndef INFORMATIONAL
+#define INFORMATIONAL 0x31
+#endif
+
+#ifndef SUCCESSFUL
+#define SUCCESSFUL 0x32
+#endif
+
+#ifndef REDIRECTION
+#define REDIRECTION 0x33
+#endif
+
+#ifndef CLIENT_ERROR
+#define CLIENT_ERROR 0x34
+#endif
+
+#ifndef SERVER_ERROR
+#define SERVER_ERROR 0x35
+#endif
 
 
 #define UNICORE_INVALID_START_LINE_ERROR -1
--- core/unicore_http_parse.cpp
+++ core/unicore_http_parse.cpp
@@ -18,8 +18,8 @@ int
    {
 
         START = 0,
-        CR_OR_LF,
-        CRLF_CONSUMED,
+        START_CR,
+        START_LF,
         START_LINE_PRECEDING_SP_FIELD,
         STATUS_LINE_START,
         STATUS_LINE_HTTP_T1_ALPHA,
@@ -93,12 +93,11 @@ int
                case 'D':
                   state = REQUEST_LINE_START;
                   break;
-               case ' ':
+               case SP:
                   state = START_LINE_PRECEDING_SP_FIELD;
                   break;
                case CR:
-               case LF:
-                  state = CR_OR_LF;
+                  state = START_CR;
                   break;
                default:
                   return UNICORE_INVALID_START_LINE_ERROR;
@@ -106,70 +105,197 @@ int
             }
             break;
          
-         // case START_LINE_PRECEDING_SP_FIELD:
-         //    switch ( ch )
-         //    {
+         case START_CR:
+            switch ( ch )
+            {
 
-         //       case 'H':
-         //          state = STATUS_LINE_START;
-         //          break;
-         //       case 'G':
-         //       case 'P':
-         //       case 'D':
-         //          state = REQUEST_LINE_START;
-         //          break;
-         //       case ' ':
-         //          break;
-         //       default:
-         //          return UNICORE_INVALID_START_LINE_ERROR;
+               case LF:
+                  state = START_LF;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+               
 
-         //    }
-         //    break;
+            }
+            break;
 
-         // case STATUS_LINE_START:
-         //    switch ( ch )
-         //    {
+         case START_LF:
+            switch ( ch )
+            {
 
-         //       case 'T':
-         //          state = STATUS_LINE_HTTP_T1_ALPHA;
-         //          break;
-         //       default:
-         //          return UNICORE_INVALID_START_LINE_ERROR;
+               case 'G':
+               case 'P':
+               case 'D':
+                  state = REQUEST_LINE_START;
+                  break;
+               case CR:
+                  state = START_CR;
+                  break;
+               case SP:
+                  state = START_LINE_PRECEDING_SP_FIELD;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+               
+            }
+            break;
 
-         //    }
-         //    break;
+         case START_LINE_PRECEDING_SP_FIELD:
+            switch ( ch )
+            {
 
-         // case STATUS_LINE_HTTP_T1_ALPHA:
-         //    switch ( ch )
-         //    {
+               case 'H':
+                  state = STATUS_LINE_START;
+                  break;
+               case 'G':
+               case 'P':
+               case 'D':
+                  state = REQUEST_LINE_START;
+                  break;
+               case SP:
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
 
-         //       case 'T':
-         //          state = STATUS_LINE_HTTP_T2_ALPHA;
-         //          break;
-         //       default:
-         //          return UNICORE_INVALID_START_LINE_ERROR;
+            }
+            break;
 
-         //    }
+         case STATUS_LINE_START:
+            switch ( ch )
+            {
 
-         // case STATUS_LINE_HTTP_T2_ALPHA:
-         //    switch ( ch )
-         //    {
+               case 'T':
+                  state = STATUS_LINE_HTTP_T1_ALPHA;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
 
-         //       case 'P':
-         //          state = STATUS_LINE_HTTP_P_ALPHA;
-         //          break;
-         //       default:
-         //          return UNICORE_INVALID_START_LINE_ERROR;
+            }
+            break;
 
-         //    }
+         case STATUS_LINE_HTTP_T1_ALPHA:
+            switch ( ch )
+            {
 
-         // case STATUS_LINE_HTTP_P_ALPHA:
-         //    switch ( ch )
-         //    {
+               case 'T':
+                  state = STATUS_LINE_HTTP_T2_ALPHA;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
 
+            }
 
+         case STATUS_LINE_HTTP_T2_ALPHA:
+            switch ( ch )
+            {
 
-         //    }
+               case 'P':
+                  state = STATUS_LINE_HTTP_P_ALPHA;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+
+         case STATUS_LINE_HTTP_P_ALPHA:
+            switch ( ch )
+            {
+
+               case '/':
+                  state = STATUS_LINE_HTTP_FORWARD_SLASH_BEFORE_VERSION;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case STATUS_LINE_HTTP_FORWARD_SLASH_BEFORE_VERSION:
+            switch ( ch )
+            {
+
+               case '1':
+                  state = STATUS_LINE_HTTP_MAJOR_VERSION_1;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case STATUS_LINE_HTTP_MAJOR_VERSION_1:
+            switch ( ch )
+            {
+
+               case '.':
+                  state = STATUS_LINE_HTTP_VERSION_DOT;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case STATUS_LINE_HTTP_VERSION_DOT:
+            switch ( ch )
+            {
+
+               case '1':
+                  state = STATUS_LINE_HTTP_MINOR_VERSION_1;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case STATUS_LINE_HTTP_MINOR_VERSION_1:
+            switch ( ch )
+            {
+
+               case SP:
+                  state  = STATUS_LINE_HTTP_VERSION_VALIDATED_BY_SP;
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case STATUS_LINE_HTTP_VERSION_VALIDATED_BY_SP:
+            switch ( ch )
+            {
+
+               case INFORMATIONAL:
+                  state = INFO_STATUS_CODE_START;
+                  break;
+               case SUCCESSFUL:
+                  state = SUCCESSFUL_STATUS_CODE_START;
+                  break;
+               case REDIRECTION:
+                  state = REDICRECTION_STATUS_CODE_START;
+                  break;
+               case CLIENT_ERROR:
+                  state = CLIENT_ERROR_STATUS_CODE_START;
+                  break;
+               case SERVER_ERROR:
+                  state = SERVER_ERROR_STATUS_CODE_START;
+                  break;
+               case SP:
+                  break;
+               default:
+                  return UNICORE_INVALID_START_LINE_ERROR;
+
+            }
+            break;
+
+         case INFO_STATUS_CODE_START:
+            switch ( ch )
+            {
+
+               
+
+            }
 
          
 
